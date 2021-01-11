5d80b27db8aa2c6eaa4865f551172d029dd15763
Mars679 <77284347+Mars679@users.noreply.github.com>
2021-01-11 16:55:32 +0200

    Add files via upload

---
+++ homeTask1_Subota.js
@@ -0,0 +1,33 @@
+'use strictly';
+
+let a = Number(prompt('Enter a: ', ' '));
+let b = Number(prompt('Enter b: ', ' '));
+
+let key = prompt('Enter operation: ', ' ');
+
+switch (key) {
+    case '+':
+        alert(a+b)
+        break;
+
+    case '-':
+        alert(a-b)
+        break;
+
+    case '/':
+        alert(a/b)
+        break;
+        
+    case '*':
+        alert(a*b)
+        break;
+
+    case '%':
+        alert(a%b)
+        break;
+
+    default:  
+    alert('Wrong operation')
+    break;
+}
+
